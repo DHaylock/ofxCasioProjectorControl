9110f59417519c45ccfdcd02d6e9c0538ca1699b
DHaylock <david.haylock@watershed.co.uk>
2016-01-18 16:52:34 +0000

    - Added: ReadMe - Updated: commands

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+ofxCasioProjectorControl
+===
+
+![addon image](doc/ofxaddons_thumbnail.png)
+
+A openFrameworks addon for Casio Projectors.
+
+If you would like to contribute to the addon feel free to fork, hack and modify the code. Also drop me a line and let me know how you end up using the addon.
+
+[@david_haylock](https://twitter.com/david_haylock)
+
+####Requirements
+
+openFrameworks v0.9.0
+
+####What the addon includes
+
+ofxCasioProjectorControl src
+
+Simple example which allows you to use the basic functionality of the addon
--- src/ofxCasioProjectorControl.h
+++ src/ofxCasioProjectorControl.h
@@ -7,58 +7,57 @@
 #include <stdlib.h>
 
 #define TIMEOUT_STEP_DURATION      250000   // In uSeconds
-#define TIMEOUT_DURATION           20       // In iterations of TIMEOUT_STEP_DURATION, 20 ~5s
+#define TIMEOUT_DURATION           1       // In iterations of TIMEOUT_STEP_DURATION, 20 ~5s
 #define NUM_BYTES 1
 #define BAUD 19200
 
-#define POWER_ON                "PWR1\r"
-#define POWER_OFF               "PWR0\r"
+#define POWER_ON                "(PWR1)\n"
+#define POWER_OFF               "(PWR0)\n"
 
-#define SET_INPUT_RGB_1         "SRC0\r"
-#define SET_INPUT_COMPONENT_1   "SRC1\r"
-#define SET_INPUT_VIDEO         "SRC2\r"
-#define SET_INPUT_RGB_2          "SRC3\r"
-#define SET_INPUT_COMPONENT_2   "SRC4\r"
-#define SET_INPUT_AUTO_1        "SRC6\r"
-#define SET_INPUT_HDMI          "SRC7\r"
-#define SET_INPUT_NETWORK       "SRC8\r"
-#define SET_INPUT_S_VIDEO       "SRC9\r"
-#define SET_INPUT_AUTO_2        "SRC10\r"
-#define SET_INPUT_FILE_VIEWER   "SRC11\r"
-#define SET_INPUT_USB_DISPLAY   "SRC12\r"
-#define SET_INPUT_CASIO_USB     "SRC13\r"
+#define SET_INPUT_RGB_1         "(SRC0)\n"
+#define SET_INPUT_COMPONENT_1   "(SRC1)\n"
+#define SET_INPUT_VIDEO         "(SRC2)\n"
+#define SET_INPUT_RGB_2         "(SRC3)\n"
+#define SET_INPUT_COMPONENT_2   "(SRC4)\n"
+#define SET_INPUT_AUTO_1        "(SRC6)\n"
+#define SET_INPUT_HDMI          "(SRC7)\n"
+#define SET_INPUT_NETWORK       "(SRC8)\n"
+#define SET_INPUT_S_VIDEO       "(SRC9)\n"
+#define SET_INPUT_AUTO_2        "(SRC10)\n"
+#define SET_INPUT_FILE_VIEWER   "(SRC11)\n"
+#define SET_INPUT_USB_DISPLAY   "(SRC12)\n"
+#define SET_INPUT_CASIO_USB     "(SRC13)\n"
+#define SET_BLANK_SCREEN_OFF    "(BLK0)\n"
+#define SET_BLANK_SCREEN_ON     "(BLK1)\n"
 
-#define SET_BLANK_SCREEN_OFF    "BLK0\r"
-#define SET_BLANK_SCREEN_ON     "BLK1\r"
+#define SET_COLOR_MODE_1        "(PST1)\n"
+#define SET_COLOR_MODE_2        "(PST2)\n"
+#define SET_COLOR_MODE_3        "(PST3)\n"
+#define SET_COLOR_MODE_4        "(PST4)\n"
+#define SET_COLOR_MODE_5        "(PST5)\n"
 
-#define SET_COLOR_MODE_1        "PST1\r"
-#define SET_COLOR_MODE_2        "PST2\r"
-#define SET_COLOR_MODE_3        "PST3\r"
-#define SET_COLOR_MODE_4        "PST4\r"
-#define SET_COLOR_MODE_5        "PST5\r"
+#define SET_ASPECT_NORMAL       "(ARZ0)\n"
+#define SET_ASPECT_16_9         "(ARZ1)\n"
+#define SET_ASPECT_4_3          "(ARZ2)\n"
+#define SET_ASPECT_LETTERBOX    "(ARZ3)\n"
+#define SET_ASPECT_FULL         "(ARZ4)\n"
+#define SET_ASPECT_TRUE         "(ARZ5)\n"
+#define SET_ASPECT_4_3_FORCED   "(ARZ6)\n"
 
-#define SET_ASPECT_NORMAL       "ARZ0\r"
-#define SET_ASPECT_16_9         "ARZ1\r"
-#define SET_ASPECT_4_3          "ARZ2\r"
-#define SET_ASPECT_LETTERBOX    "ARZ3\r"
-#define SET_ASPECT_FULL         "ARZ4\r"
-#define SET_ASPECT_TRUE         "ARZ5\r"
-#define SET_ASPECT_4_3_FORCED   "ARZ6\r"
+#define GET_LAMP_HOURS          "(LMP)\n"
 
-#define GET_LAMP_HOURS          "LMP\r"
+#define SET_FREEZE_ON           "(FRZ1)\n"
+#define SET_FREEZE_OFF          "(FRZ2)\n"
 
-#define SET_FREEZE_ON           "FRZ1\r"
-#define SET_FREEZE_OFF          "FRZ2\r"
+#define MODE_FRONT              "(POS0)\n"
+#define MODE_REAR               "(POS1)\n"
+#define MODE_FRONT_CEILING      "(POS2)\n"
+#define MODE_REAR_CEILING       "(POS3)\n"
 
-#define MODE_FRONT              "POS0\r"
-#define MODE_REAR               "POS1\r"
-#define MODE_FRONT_CEILING      "POS2\r"
-#define MODE_REAR_CEILING       "POS3\r"
+#define SET_BLANK_SCREEN_OFF    "(BLK0)\n"
+#define SET_BLANK_SCREEN_ON     "(BLK1)\n"
 
-#define SET_BLANK_SCREEN_OFF    "BLK0\r"
-#define SET_BLANK_SCREEN_ON     "BLK1\r"
-
-#define GET_ERROR_MESSAGE       "STS\r"
+#define GET_ERROR_MESSAGE       "(STS)\n"
 
 //--------------------------------------------------------------
 class ofxCasioProjectorControl : public ofSerial {
